01b625f3143fa2795e3e15e69edfa4d516a70733
Nana Eka Wulandari <nana.wulandari@si.ukdw.ac.id>
2019-10-30 22:36:38 +0700

    second commit

--- app/src/main/java/com/example/proteintracker/DaftarTemanActivity.java
+++ app/src/main/java/com/example/proteintracker/DaftarTemanActivity.java
@@ -32,10 +32,10 @@ public class DaftarTemanActivity extends AppCompatActivity {
     }
     private  void addData(){
         Daftartemanarrylist =  new ArrayList<>();
-        Daftartemanarrylist.add(new DaftarTeman(" nama : Nikolaus Aryawan Ravato Wijaya","nim :72170115"," hobby : Barongsai","cita2 : Membahagiakan dan menyangkan Ortu","moto :Terus berproses walaupun itu berat","gender : Laki-aki",R.drawable.arry));
-        Daftartemanarrylist.add(new DaftarTeman("nama : Jonathan Prajna Marga Parama","nim : 72170094"," hobby : Kerja Lapangan","cita2 : konglomerat","moto : apapun yang kuperbuat dibuat Tuhan berhasil"," gender : Laki-Laki",R.drawable.jona));
-        Daftartemanarrylist.add(new DaftarTeman(" nama : Nana Eka Wulandari"," nim : 72170090","hobby : Tidur","cita2 : Kaya raya","moto : jadi diri sendiri","gender : Perempuan",R.drawable.nana));
-        Daftartemanarrylist.add(new DaftarTeman("nama : Beni Mulia Tabarus","nim : 72170177","hobby : Basket dan Musik","cita2 : membahagiakan ortu","moto : just dot it","gender :Laki-laki",R.drawable.beni));
+        Daftartemanarrylist.add(new DaftarTeman("Grace Hutabarat", "72170171", "Nonton", "Good Leader", "Bertumbuh dalam proses", "Perempuan", R.drawable.grace));
+        Daftartemanarrylist.add(new DaftarTeman("Jonathan Prajna Marga Parama", "72170094", "Kerja Lapangan", "konglomerat", "apapun yang kuperbuat dibuat Tuhan berhasil", "Laki-Laki", R.drawable.jona));
+        Daftartemanarrylist.add(new DaftarTeman("Valeriana Tanesha Indra S", "72170143", "Baca Novel", "Bahagia", "Shoot for the moon because even if you miss, you'll land among the stars", "Perempuan", R.drawable.vale));
+        Daftartemanarrylist.add(new DaftarTeman("Beni Mulia Tabarus", "72170177", "Basket dan Musik", "membahagiakan ortu", "just dot it", "Laki-laki", R.drawable.beni));
     }
 
 
